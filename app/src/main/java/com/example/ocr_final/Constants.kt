438845bfa79a1680942f5f60e07aa9b28c5b8be7
Gambhir Dhruv <75164918+dhruv-gambhir@users.noreply.github.com>
2024-07-01 11:31:06 +0800

    Update Constants.kt

--- app/src/main/java/com/example/ocr_final/Constants.kt
+++ app/src/main/java/com/example/ocr_final/Constants.kt
@@ -2,7 +2,7 @@ package com.example.ocr_final
 
 class Constants {
     companion object {
-        const val REGEX =  "(?:.*?:\\s*|\\s+)?(?=.*\\d)([\\w\\d-]{8,})\\b";
+        const val REGEX =  "(?:.*?:\\s*|\\s+)?(?=.*\\d)([\\w\\d-]{8,})\\b"; // refer to regex documentation
         val vend_list = listOf("batch", "lot", "p.o.");
         val inhouse_list = listOf("vend")
     }
